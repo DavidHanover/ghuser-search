7d0f9595174aa49c373d0aa028461eb6b497639d
DavidHanover <DavidHanover89@gmail.com>
2020-01-17 15:21:29 -0800

    refactored UserItem.js and Navbar.js to be functional components instead of classes

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,26 +1,23 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-export class Navbar extends Component {
-  static defaultProps = {
-    title: "GitHub Finder",
-    icon: "fab fa-github"
-  };
+const Navbar = props => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={props.icon}></i> {props.title}
+      </h1>
+    </nav>
+  );
+};
+Navbar.defaultProps = {
+  title: "GitHub Finder",
+  icon: "fab fa-github"
+};
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-  };
-
-  render() {
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className={this.props.icon}></i> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
 
 export default Navbar;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,34 +1,32 @@
 import React, { Component } from "react";
 
-class UserItem extends Component {
-  static defaultProps = {
-    user: {
-      id: 0,
-      username: "null user",
-      avatar_url:
-        "https://iupac.org/wp-content/uploads/2018/05/default-avatar.png",
-      profile_url: "https://github.com"
-    }
-  };
-  render() {
-    const { username, avatar_url, profile_url } = this.props.user;
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt={username + " avatar image"}
-          className="round-img"
-          style={{ width: "60px" }}
-        />
-        <h4>{username}</h4>
-        <div>
-          <a href={profile_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
+const UserItem = props => {
+  const { username, avatar_url, profile_url } = props.user;
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt={username + " avatar image"}
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h4>{username}</h4>
+      <div>
+        <a href={profile_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
+UserItem.defaultProps = {
+  user: {
+    id: 0,
+    username: "null user",
+    avatar_url:
+      "https://iupac.org/wp-content/uploads/2018/05/default-avatar.png",
+    profile_url: "https://github.com"
+  }
+};
 export default UserItem;
