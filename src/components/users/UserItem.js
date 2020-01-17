d6519dc94d01ad65d070c475f20f075c2fe2a7d3
DavidHanover <DavidHanover89@gmail.com>
2020-01-17 13:09:51 -0800

    wrote in some dummy user information into the UserItem state

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 
 class UserItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: 1,
+      login: "mojombo",
+      avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+      html_url: "https://github.com/mojombo"
+    };
+    console.log(this.state);
+  }
   render() {
     return (
-      <div>
-        <div>User Item</div>
+      <div className="card text-center">
+        <img
+          src={this.state.avatar_url}
+          alt={this.state.login + " avatar image"}
+        />
       </div>
     );
   }
