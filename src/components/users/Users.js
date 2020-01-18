1cea3d8732481fc5cc8da42b198edfcec3e054bf
DavidHanover <DavidHanover89@gmail.com>
2020-01-17 16:47:39 -0800

    added axios.  replaced Users.js in-state array with async/awai api call to api.github.com/users.  refactored Users.js to be a functional component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,45 +1,20 @@
 import React, { Component } from "react";
 import UserItem from "./UserItem";
 
-export class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 1,
-        username: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        profile_url: "https://github.com/mojombo"
-      },
-      {
-        id: 2,
-        username: "defunkt",
-        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-        profile_url: "https://github.com/defunkt"
-      },
-      {
-        id: 3,
-        username: "pjhyett",
-        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-        profile_url: "https://github.com/pjhyett"
-      }
-    ]
+const Users = ({ users }) => {
+  const userStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(3,1fr)",
+    gridGap: "1rem"
   };
-  render() {
-    const { users } = this.state;
-    return (
-      <div style={userStyle}>
-        {users.map(user => {
-          return <UserItem user={user} key={user.id} />;
-        })}
-      </div>
-    );
-  }
-}
 
-const userStyle = {
-  display: "grid",
-  gridTemplateColumns: "repeat(3,1fr)",
-  gridGap: "1rem"
+  return (
+    <div style={userStyle}>
+      {users.map(user => {
+        return <UserItem user={user} key={user.id} />;
+      })}
+    </div>
+  );
 };
 
 export default Users;
